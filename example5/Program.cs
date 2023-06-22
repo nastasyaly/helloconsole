efc06f42f01fe8213f82ce2870d525de5798bb66
Anastasia Polivanova <lyaminana@gmail.com>
2023-06-22 16:05:37 +0300

    добавили условие приветствия

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "Настасья")
+{
+    Console.WriteLine("Ура, Настасья!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
